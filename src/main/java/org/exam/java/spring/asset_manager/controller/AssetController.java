c3df50f819f7d3494edbd778f07e1bcf1de8bfb0
saverio mandarano <saverio.mandarano1@gmail.com>
2026-06-11 20:46:14 +0200

    implement search filters for Asset entity

--- src/main/java/org/exam/java/spring/asset_manager/controller/AssetController.java
+++ src/main/java/org/exam/java/spring/asset_manager/controller/AssetController.java
@@ -1,5 +1,6 @@
 package org.exam.java.spring.asset_manager.controller;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.exam.java.spring.asset_manager.model.Asset;
@@ -12,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/assets")
@@ -39,5 +41,75 @@ public class AssetController {
         model.addAttribute("content", "assets/show");
         return "layout/main";
     }
+
+    @GetMapping("/searchByName")
+    public String searchByName(@RequestParam(name = "name") String name, Model model) {
+
+        List<Asset> assets;
+
+        if (name != null && !name.isBlank()) {
+            assets = assetService.findByName(name);
+        } else {
+            assets = assetService.findAll();
+        }
+
+        model.addAttribute("assets", assets);
+
+        model.addAttribute("content", "assets/index");
+        return "layout/main";
+    }
+
+    @GetMapping("/searchByTicker")
+    public String searchByTicker(@RequestParam(name = "ticker") String ticker, Model model) {
+
+        List<Asset> assets;
+
+        if (ticker != null && !ticker.isBlank()) {
+            assets = assetService.findByTicker(ticker);
+        } else {
+            assets = assetService.findAll();
+        }
+
+        model.addAttribute("assets", assets);
+
+        model.addAttribute("content", "assets/index");
+        return "layout/main";
+    }
+
+    // @GetMapping("/searchByPrice")
+    // public String searchByPrice(@RequestParam(name = "price") BigDecimal price,
+    // Model model) {
+
+    // List<Asset> assets;
+
+    // if (price != null) {
+    // assets = assetService.findByPrice(price);
+    // } else {
+    // assets = assetService.findAll();
+    // }
+
+    // model.addAttribute("assets", assets);
+
+    // return "assets/index";
+    // }
+
+    @GetMapping("/searchByTickerOrName")
+    public String searchByTickerOrName(@RequestParam(name = "query") String query, Model model) {
+
+        List<Asset> assets;
+
+        if (query != null && !query.isBlank()) {
+            assets = assetService.findByTickerOrName(query);
+        } else {
+            assets = assetService.findAll();
+        }
+
+        model.addAttribute("assets", assets);
+
+        model.addAttribute("content", "assets/index");
+        return "layout/main";
+    }
+
+    // CREATE
 
 }
