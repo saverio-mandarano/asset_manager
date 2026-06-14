74dff4b33ad075d004fad3b91dd268db51ccef67
saverio mandarano <saverio.mandarano1@gmail.com>
2026-06-14 14:05:48 +0200

    Implement crud methos for tag entity. Fix fragment warnings in layout/main.html

--- src/main/java/org/exam/java/spring/asset_manager/controller/TagController.java
+++ src/main/java/org/exam/java/spring/asset_manager/controller/TagController.java
@@ -3,65 +3,163 @@ package org.exam.java.spring.asset_manager.controller;
 import java.util.List;
 
 import org.exam.java.spring.asset_manager.model.Tag;
-import org.springframework.boot.data.autoconfigure.web.DataWebProperties.Sort;
+import org.exam.java.spring.asset_manager.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.data.domain.Sort;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/tags")
 public class TagController {
 
-    // @GetMapping
-    // public String index(
-    // @RequestParam(name = "sortBy", required = false) String sortBy,
-    // @RequestParam(name = "direction", required = false) String direction,
-    // Model model) {
+    @Autowired
+    private TagService tagService;
 
-    // if (direction == null
-    // || !(direction.equalsIgnoreCase("asc") ||
-    // direction.equalsIgnoreCase("desc"))) {
-    // direction = "asc";
-    // }
+    @GetMapping
+    public String index(
+            @RequestParam(name = "sortBy", required = false) String sortBy,
+            @RequestParam(name = "direction", required = false) String direction,
+            Model model) {
 
-    // Sort.Direction sortDirection = direction.equalsIgnoreCase("desc") ?
-    // Sort.Direction.DESC
-    // : Sort.Direction.ASC;
-    // List<Tag> tags;
+        if (direction == null
+                || !(direction.equalsIgnoreCase("asc") || direction.equalsIgnoreCase("desc"))) {
+            direction = "asc";
+        }
 
-    // if (sortBy != null) {
-    // switch (sortBy) {
-    // case "name":
-    // categories = categoryService.findAllSorted("name", sortDirection);
-    // break;
-    // case "riskLevel":
-    // // categories = categoryService.findAllSorted("riskLevel", sortDirection);
-    // //
-    // categories = direction.equalsIgnoreCase("asc")
-    // ? categoryService.findAllOrderByRiskLevelAsc()
-    // : categoryService.findAllOrderByRiskLevelDesc();
-    // break;
-    // case "assetsCount":
-    // categories = direction.equalsIgnoreCase("asc")
-    // ? categoryService.findAllOrderByAssetsCountAsc()
-    // : categoryService.findAllOrderByAssetsCountDesc();
-    // break;
-    // default:
-    // categories = categoryService.findAll();
-    // sortBy = null;
-    // }
-    // } else {
-    // categories = categoryService.findAll();
-    // }
+        Sort.Direction sortDirection = direction.equalsIgnoreCase("desc") ? Sort.Direction.DESC
+                : Sort.Direction.ASC;
+        List<Tag> tags;
 
-    // model.addAttribute("categories", categories);
-    // model.addAttribute("pageTitle", "Category list");
-    // model.addAttribute("sortBy", sortBy);
-    // model.addAttribute("direction", direction);
+        if (sortBy != null) {
+            switch (sortBy) {
+                case "name":
+                    tags = tagService.findAllSorted("name", sortDirection);
+                    break;
+                case "type":
+                    // tags = tagService.findAllSorted("type", sortDirection);
+                    //
+                    tags = direction.equalsIgnoreCase("asc")
+                            ? tagService.findAllOrderByTypeAsc()
+                            : tagService.findAllOrderByTypeDesc();
+                    break;
+                case "assetsCount":
+                    tags = direction.equalsIgnoreCase("asc")
+                            ? tagService.findAllOrderByAssetsCountAsc()
+                            : tagService.findAllOrderByAssetsCountDesc();
+                    break;
+                default:
+                    tags = tagService.findAll();
+                    sortBy = null;
+            }
+        } else {
+            tags = tagService.findAll();
+        }
 
-    // model.addAttribute("content", "categories/index");
-    // return "layout/main";
-    // }
+        model.addAttribute("tags", tags);
+        model.addAttribute("pageTitle", "Tag list");
+        model.addAttribute("sortBy", sortBy);
+        model.addAttribute("direction", direction);
+
+        model.addAttribute("content", "tags/index");
+        return "layout/main";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("tag", tagService.findById(id));
+        model.addAttribute("pageTitle", tagService.findById(id).getName() + " details");
+
+        model.addAttribute("content", "tags/show");
+        return "layout/main";
+    }
+
+    @GetMapping("/searchByName")
+    public String searchByName(@RequestParam(name = "name") String name, Model model) {
+        List<Tag> tags;
+
+        if (name != null && !name.isBlank()) {
+            tags = tagService.findByName(name);
+        } else {
+            tags = tagService.findAll();
+        }
+
+        model.addAttribute("tags", tags);
+        model.addAttribute("pageTitle", "Tag list");
+
+        model.addAttribute("content", "tags/index");
+        return "layout/main";
+    }
+
+    // CREATE
+    @GetMapping("/create")
+    // public String create(Authentication authentication, Model model) {
+    public String create(Model model) {
+        model.addAttribute("Tag", new Tag());
+        model.addAttribute("pageTitle", "Create tag");
+
+        model.addAttribute("content", "tags/create-or-edit");
+        return "layout/main";
+
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("tag") Tag formTag,
+            BindingResult bindingResult, Model model) {
+        model.addAttribute("pageTitle", "Create tag");
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("content", "tags/create-or-edit");
+            return "layout/main";
+        }
+
+        tagService.create(formTag);
+        return "redirect:/tags";
+    }
+
+    // Update
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("tag", tagService.findById(id));
+        model.addAttribute("edit", true);
+        model.addAttribute("pageTitle", "Edit " + tagService.findById(id).getName());
+
+        model.addAttribute("content", "tags/create-or-edit");
+        return "layout/main";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute("tag") Tag formTag,
+            BindingResult bindingResult, Model model) {
+        model.addAttribute("pageTitle", "Edit " + formTag.getName());
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("edit", true);
+            model.addAttribute("content", "tags/create-or-edit");
+            return "layout/main";
+        }
+
+        tagService.update(formTag);
+        return "redirect:/tags/" + formTag.getId();
+    }
+
+    // DELETE
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id,
+            RedirectAttributes redirectAttributes) {
+
+        tagService.deleteById(id);
+
+        return "redirect:/tags";
+    }
 }
--- src/main/java/org/exam/java/spring/asset_manager/service/TagService.java
+++ src/main/java/org/exam/java/spring/asset_manager/service/TagService.java
@@ -1,5 +1,95 @@
 package org.exam.java.spring.asset_manager.service;
 
-public class TagService {
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.exam.java.spring.asset_manager.model.Asset;
+import org.exam.java.spring.asset_manager.model.Tag;
+import org.exam.java.spring.asset_manager.repository.AssetRepository;
+import org.exam.java.spring.asset_manager.repository.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Sort;
+
+@Service
+public class TagService {
+    @Autowired
+    private TagRepository tagRepository;
+
+    @Autowired
+    private AssetRepository assetRepository;
+
+    // READ
+    public List<Tag> findAll() {
+        return tagRepository.findAll();
+    }
+
+    public List<Tag> findAllSorted(String property, Sort.Direction direction) {
+        return tagRepository.findAll(Sort.by(direction, property));
+    }
+
+    public List<Tag> findByName(String name) {
+        return tagRepository.findByNameContainingIgnoreCase(name);
+    }
+
+    public Tag findById(Integer id) {
+        Optional<Tag> tagAttempt = tagRepository.findById(id);
+        if (tagAttempt.isEmpty()) {
+            throw new RuntimeException("Tag not found with id: " + id);
+        }
+        return tagAttempt.get();
+    }
+
+    public List<Tag> findAllOrderByAssetsCountAsc() {
+        return tagRepository.findAll()
+                .stream()
+                .sorted(Comparator.comparingInt(c -> c.getAssets().size()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Tag> findAllOrderByAssetsCountDesc() {
+        return tagRepository.findAll()
+                .stream()
+                .sorted(Comparator.comparingInt((Tag c) -> c.getAssets().size()).reversed())
+                .collect(Collectors.toList());
+    }
+
+    public List<Tag> findAllOrderByTypeAsc() {
+        return tagRepository.findAll()
+                .stream()
+                .sorted(Comparator.comparingInt(c -> c.getAssets().size()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Tag> findAllOrderByTypeDesc() {
+        return tagRepository.findAll()
+                .stream()
+                .sorted(Comparator.comparingInt((Tag c) -> c.getAssets().size()).reversed())
+                .collect(Collectors.toList());
+    }
+
+    // CREATE
+    public Tag create(Tag tag) {
+        return tagRepository.save(tag);
+    }
+
+    public Tag update(Tag tag) {
+        return tagRepository.save(tag);
+    }
+
+    // DELETE
+    public void deleteById(Integer tagId) {
+        Tag tag = tagRepository.findById(tagId)
+                .orElseThrow(() -> new RuntimeException("Tag not found with id: " + tagId));
+
+        for (Asset asset : tag.getAssets()) {
+            if (asset.getTags() != null) {
+                asset.getTags().remove(tag);
+            }
+        }
+
+        tagRepository.deleteById(tagId);
+    }
 }
