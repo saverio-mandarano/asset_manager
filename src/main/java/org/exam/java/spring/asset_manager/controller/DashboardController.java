1e2c4e1a3faac501d64b150d70e966c608dee2e5
saverio mandarano <saverio.mandarano1@gmail.com>
2026-06-15 12:43:04 +0200

    HomePage controller

--- src/main/java/org/exam/java/spring/asset_manager/controller/DashboardController.java
+++ src/main/java/org/exam/java/spring/asset_manager/controller/DashboardController.java
@@ -1,5 +1,14 @@
 package org.exam.java.spring.asset_manager.controller;
 
+import java.util.List;
+
+import org.exam.java.spring.asset_manager.model.Asset;
+import org.exam.java.spring.asset_manager.model.Category;
+import org.exam.java.spring.asset_manager.model.Tag;
+import org.exam.java.spring.asset_manager.service.AssetService;
+import org.exam.java.spring.asset_manager.service.CategoryService;
+import org.exam.java.spring.asset_manager.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +18,48 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class DashboardController {
 
+    @Autowired
+    private AssetService assetService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private TagService tagService;
+
     @GetMapping
     public String index(Model model) {
-        model.addAttribute("content", "dashboards/index");
+        List<Asset> assets = assetService.findAll();
+        List<Category> categories = categoryService.findAll();
+        List<Tag> tags = tagService.findAll();
 
+        model.addAttribute("assets", assets);
+        model.addAttribute("categories", categories);
+        model.addAttribute("tags", tags);
+
+        model.addAttribute("assetCount", assets.size());
+        model.addAttribute("categoryCount", categories.size());
+        model.addAttribute("tagCount", tags.size());
+
+        model.addAttribute("pageTitle", "Backoffice Dashboard");
+
+        int uncategorized = 0;
+        for (Asset asset : assets) {
+            if (asset.getCategory() == null) {
+                uncategorized++;
+            }
+        }
+        model.addAttribute("uncategorized", uncategorized);
+
+        int untagged = 0;
+        for (Asset asset : assets) {
+            if (asset.getTags() == null || asset.getTags().isEmpty()) {
+                untagged++;
+            }
+        }
+        model.addAttribute("untagged", untagged);
+
+        model.addAttribute("content", "dashboards/index");
         return "layout/main";
     }
-
 }
